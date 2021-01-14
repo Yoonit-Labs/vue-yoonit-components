815dfefed0bef9c824013a5fee1f0e76ebc692d6
sabraas <sabrinaangelimsampaio@gmail.com>
2021-01-14 15:37:32 -0300

    Adding YooSwitchCard

---
+++ src/components/molecules/YooSwitchCard/YooSwitchCard.test.js
@@ -0,0 +1,55 @@
+import { shallowMount } from '@vue/test-utils'
+import YooSwitchCard from './YooSwitchCard.vue'
+
+const classBlock = 'yoo-switch-card'
+const SlotText = 'Default Slot Text'
+
+const mountComponent = () => {
+  return shallowMount(YooSwitchCard, {
+    slots: { default: SlotText },
+    propsData: {
+      text: SlotText
+    }
+  })
+}
+
+describe('YooSwitchCard Component', () => {
+  let wrapper
+  beforeEach(() => {
+    wrapper = mountComponent()
+  })
+
+  it('Matches Snapshot', async () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('Loads the Component HTML', () => {
+    expect(wrapper.classes('yoo-switch-card')).toBe(true)
+    expect(wrapper.find('.yoo-switch-card__checkbox').exists()).toBe(true)
+  })
+
+  describe('Props', () => {
+    describe('checked', () => {
+      it('Has a valid default value', () => {
+        expect(YooSwitchCard.props.checked.default).toBe(false)
+      })
+      it(`Includes checked class: .${classBlock}--active when checked`, async () => {
+        await wrapper.setProps({ checked: true })
+        expect(wrapper.find(`.${classBlock}--active`).exists()).toBe(true)
+      })
+      it(`Does not include checked class:.${classBlock}--active when not checked`, async () => {
+        await wrapper.setProps({ checked: false })
+        expect(wrapper.find(`.${classBlock}--active`).exists()).toBe(false)
+      })
+    })
+  }) // describe Props
+
+  describe('Events', () => {
+    describe('doCheck', () => {
+      it('Emits doCheck Event', async () => {
+        await wrapper.find(`.${classBlock}__checkbox`).vm.$emit('response')
+        expect(wrapper.emitted()).toHaveProperty('response')
+      })
+    })
+  }) // describe Events
+})
