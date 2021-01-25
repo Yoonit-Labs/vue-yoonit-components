74afb36c2452eec2134d0035ec82fa3dc8528050
vitoriacosta <viitoriacoosta@gmail.com>
2021-01-25 20:28:06 -0300

    Update index in components folder

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,59 @@
-import yooButton from './atoms/YooButton/YooButton.vue'
+import YooAnimation from './atoms/YooAnimation/YooAnimation.vue'
+import YooAvatar from './atoms/YooAvatar/YooAvatar.vue'
+import YooButton from './atoms/YooButton/YooButton.vue'
+import YooCheckButton from './atoms/YooCheckButton/YooCheckButton.vue'
+import YooIcon from './atoms/YooIcon/YooIcon.vue'
+import YooSeparator from './atoms/YooSeparator/YooSeparator.vue'
+import YooStepper from './atoms/YooButton/YooStepper.vue'
+import YooWarning from './atoms/YooWarning/YooWarning.vue'
+
+import YooButtonToggle from './molecules/YooButtonToggle/YooButtonToggle.vue'
+import YooCard from './molecules/YooCard/YooCard.vue'
+import YooFormGroup from './molecules/YooFormGroup/YooFormGroup.vue'
+import YooHeader from './molecules/YooHeader/YooHeader.vue'
+import YooModal from './molecules/YooModal/YooModal.vue'
+import YooNotify from './molecules/YooNotify/YooNotify.vue'
+import YooPagination from './molecules/YooPagination/YooPagination.vue'
+import YooPostCard from './molecules/YooPostCard/YooPostCard.vue'
+import YooQuizQuestion from './molecules/YooQuizQuestion/YooQuizQuestion.vue'
+import YooRateButton from './molecules/YooRateButton/YooRateButton.vue'
+import YooSegmentedBar from './molecules/YooSegmentedBar/YooSegmentedBar.vue'
+import YooSwitch from './molecules/YooSwitch/YooSwitch.vue'
+import YooSwitchCard from './molecules/YooSwitchCard/YooSwitchCard.vue'
+import YooTableAttribute from './molecules/YooTableAttribute/YooTableAttribute.vue'
+
+import YooFlexLayout from './quarks/YooFlexLayout/YooFlexLayout.vue'
+import YooGridLayout from './quarks/YooGridLayout/YooGridLayout.vue'
+import YooIndicator from './quarks/YooFlexLayout/YooIndicator.vue'
+import YooScrollView from './quarks/YooScrollView/YooScrollView.vue'
 
 const yooComponents = {
-  yooButton
+  YooButton,
+  YooAnimation,
+  YooAvatar,
+  YooCheckButton,
+  YooIcon,
+  YooSeparator,
+  YooStepper,
+  YooWarning,
+  YooButtonToggle,
+  YooCard,
+  YooFormGroup,
+  YooHeader,
+  YooModal,
+  YooNotify,
+  YooPagination,
+  YooPostCard,
+  YooQuizQuestion,
+  YooRateButton,
+  YooSegmentedBar,
+  YooSwitch,
+  YooSwitchCard,
+  YooTableAttribute,
+  YooFlexLayout,
+  YooGridLayout,
+  YooIndicator,
+  YooScrollView
 }
 
 export default yooComponents
