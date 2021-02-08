c3d84bf3340a3c7c36e8e24da5fa8d35d4a963ad
Fernando Junior <fernando.junior85@outlook.com>
2021-02-08 11:10:30 -0300

    Adjustment YooDropdown in storybook

---
+++ src/components/atoms/Dropdown/Dropdown.stories.js
@@ -0,0 +1,38 @@
+import YooDropdown from '@/components/atoms/Dropdown/Dropdown.vue'
+import PropsConfig from '@/components/atoms/Dropdown/Dropdown.config'
+
+export default {
+  title: 'Yoonit/YooDropdown',
+  component: YooDropdown,
+  argTypes: {
+    size: {
+      control: {
+        type: 'select',
+        options: PropsConfig.size.options
+      }
+    },
+    options: {
+      type: 'array',
+      options: []
+    }
+  }
+}
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { YooDropdown },
+  template:
+    '<yoo-dropdown v-bind="$props"></yoo-dropdown>'
+})
+
+export const Custom = Template.bind({})
+Custom.args = { size: 'medium', options: [{ label: 'YooDropDown', value: '1' }, { label: 'YooDropDown 2', value: '2' }, { label: 'YooDropDown 3', value: '3' }], selected: { label: 'YooDropDown', value: '1' } }
+
+export const Small = Template.bind({})
+Small.args = { size: 'small', options: [{ label: 'YooDropDown', value: '1' }, { label: 'YooDropDown 2', value: '2' }], selected: { label: 'YooDropDown', value: '1' } }
+
+export const Medium = Template.bind({})
+Medium.args = { size: 'medium', options: [{ label: 'YooDropDown', value: '1' }, { label: 'YooDropDown 2', value: '2' }], selected: { label: 'YooDropDown', value: '1' } }
+
+export const Large = Template.bind({})
+Large.args = { size: 'large', options: [{ label: 'YooDropDown', value: '1' }, { label: 'YooDropDown 2', value: '2' }], selected: { label: 'YooDropDown', value: '1' } }
---
+++ src/components/atoms/Dropdown/Dropdown.vue
@@ -0,0 +1,123 @@
+<template lang="pug">
+.dropdown(
+  :class="['dropdowns', ...takeModifier]"
+)
+  .dropdown__select(
+    v-if="!showOptions"
+    @click="doShowOptions"
+  )
+    .select
+      p.select__text {{takeSelectedOptionLabel}}
+
+  .dropdown__options(
+    v-if="showOptions"
+  )
+    .option(
+        v-for="(option, index) in takeOptions",
+        :key="index"
+        @click="doSelectLabelChange(option.label, option.value)"
+    )
+      p.option__text {{option.label}}
+</template>
+
+<script>
+
+export default {
+  name: 'YooDropDown',
+  props: {
+    options: {
+      type: Array,
+      required: true,
+      default () {
+        return []
+      }
+    },
+    selected: {
+      type: Object,
+      required: false
+    },
+    size: {
+      type: String,
+      default: 'medium',
+      validator (value) {
+        return [
+          'small',
+          'medium',
+          'large'
+        ].indexOf(value) !== -1
+      }
+    }
+  },
+  data: () => ({
+    selectLabel: {},
+    showOptions: false
+  }),
+  created () {},
+  mounted () {
+    this.selectLabel = this.selected || this.takeOptions[0]
+  },
+  updated () {},
+  beforeDestroy () {
+    // document.removeEventListener(
+    //     '',
+    //     event =>
+    //         this.functionName(event)
+    // )
+  },
+  destroyed () {},
+  components: {},
+  computed: {
+    takeSelectedOptionLabel () {
+      if (this.selectLabel && this.selectLabel.label) {
+        return this.selectLabel.label
+      }
+      return this.takeOptions[0].label
+    },
+    takeOptions () {
+      const hasSelectLabel = Object.keys(this.selectLabel).length
+      if (hasSelectLabel !== 0) {
+        const arr = [...this.options]
+        arr.unshift(
+          arr.splice(
+            arr.findIndex(
+              elm => (elm.label === this.selectLabel.label && elm.value === this.selectLabel.value)),
+            1)[0]
+        )
+        return arr
+      }
+      return this.options
+    },
+    takeModifier () {
+      const block = 'dropdown'
+      const classList = []
+
+      if (this.size && (this.size !== 'medium')) {
+        classList
+          .push(
+            `${block}--${this.size}`
+          )
+      }
+
+      return classList
+    }
+  },
+  methods: {
+    doShowOptions () {
+      this.showOptions = !this.showOptions
+    },
+    doSelectLabelChange (label, value) {
+      if (label && value) {
+        this.selectLabel = Object.assign({ label, value })
+        this.$emit('onClick', value)
+      }
+      this.doShowOptions()
+    }
+  },
+  filters: {},
+  watch: {}
+}
+</script>
+
+<style src="./Dropdown.sass" scoped lang="sass">
+
+</style>
