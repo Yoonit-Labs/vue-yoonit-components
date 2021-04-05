219e165d009e1bcf7b2e9f95c0f3fb7568cee06c
Gabriel Santiago Cardoso Rizzo <gabriel96.gsr@gmail.com>
2021-04-05 16:35:14 -0300

    Updating FormGroup tests

--- src/components/molecules/FormGroup/FormGroup.test.js
+++ src/components/molecules/FormGroup/FormGroup.test.js
@@ -1,6 +1,5 @@
-import { shallowMount, mount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import YooFormGroup from '@/components/molecules/FormGroup/FormGroup.vue'
-
 const mountComponent = () => {
   return shallowMount(YooFormGroup, {
     propsData: {
@@ -8,24 +7,23 @@ const mountComponent = () => {
       label: 'Label',
       value: 'Text',
       placeholder: 'Placeholder'
-    }
+    },
+    data: () => ({
+      userHasTyped: true
+    })
   })
 }
-
 describe('YooFormGroup Component', () => {
   let wrapper
   beforeEach(() => {
     wrapper = mountComponent()
   })
-
   it('Matches Snapshot', () => {
     expect(wrapper).toMatchSnapshot()
   })
-
   it('Loads the Component HTML', () => {
     expect(wrapper.find('input').exists()).toBe(true)
   })
-
   describe('Props', () => {
     describe('Disabled mode', () => {
       it('Check if this input is disabled', async () => {
@@ -36,60 +34,37 @@ describe('YooFormGroup Component', () => {
             placeholder: 'Placeholder'
           }
         })
-
         expect(wrapper.find('input').element.disabled).toBeTruthy()
       })
     })
-
     describe('isValid Test', () => {
       it('If not isValid state', async () => {
-        await wrapper.setData({ isValid: false })
+        await wrapper.setProps({ isValid: false })
         expect(wrapper.find('label').classes().includes('yoo-input--error')).toBe(true)
       })
-
       it('If isValid state', async () => {
-        await wrapper.setData({ isValid: true })
+        await wrapper.setProps({ isValid: true })
         expect(wrapper.find('label').classes().includes('yoo-input--success')).toBe(true)
       })
     })
-
     describe('Required Test', () => {
       it('If required and text is empty', async () => {
-        await mount(YooFormGroup, {
-          propsData: {
-            required: true,
-            label: 'Label',
-            value: '',
-            placeholder: 'Placeholder'
-          }
-        })
-        await wrapper.setProps({ validateOnDataInput: false, value: '', required: true })
+        await wrapper.setProps({ validateOnDataInput: false, value: '', required: true, isValid: false })
         await wrapper.find('input').trigger('blur')
         expect(wrapper.find('label').classes().includes('yoo-input--error')).toBe(true)
       })
-
       it('If required false', async () => {
-        await mount(YooFormGroup, {
-          propsData: {
-            required: false,
-            label: 'Label',
-            value: '',
-            placeholder: 'Placeholder'
-          }
-        })
-        await wrapper.setProps({ validateOnDataInput: false, value: '', required: false })
+        await wrapper.setProps({ validateOnDataInput: false, value: '', required: false, isValid: true })
         await wrapper.find('input').trigger('blur')
         expect(wrapper.find('label').classes().includes('yoo-input--error')).toBe(false)
       })
-
       it('If required and has text', async () => {
-        await wrapper.setProps({ value: 'Any text' })
+        await wrapper.setProps({ value: 'Any text', isValid: true })
         expect(wrapper.vm.required).toBeTruthy()
         await wrapper.find('input').trigger('blur')
         expect(wrapper.find('label').classes().includes('yoo-input--success')).toBe(true)
       })
     })
-
     describe('Required Rule', () => {
       it('Required rule is executed', async () => {
         await wrapper.setProps({
@@ -107,12 +82,10 @@ describe('YooFormGroup Component', () => {
           }
         })
         await wrapper.find('input').setValue('a')
-
         expect(wrapper.find('label').classes().includes('yoo-input--error')).toBe(true)
       })
     })
   }) // describe Props
-
   describe('Events', () => {
     describe('On input text', () => {
       it('Emits keypress Event', async () => {
