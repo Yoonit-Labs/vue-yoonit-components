11ac19ccf890d32c4e2de3da8da25590014a9cd0
Gabriel Santiago Cardoso Rizzo <gabriel96.gsr@gmail.com>
2022-02-22 19:22:27 -0300

    Adding new tests to camera

--- src/components/molecules/Camera/Camera.test.js
+++ src/components/molecules/Camera/Camera.test.js
@@ -87,5 +87,81 @@ describe('Testing YooCamera', () => {
 
       expect(YooCamera.computed.takeStream.call(localThis).video.facingMode).toBe(PropsConfig.facingMode.default)
     })
+
+    it('Should set canvas size correctly', () => {
+      const wrapper = mountDefaultYooCamera(mount)
+
+      expect(Object.keys(wrapper.vm.canvasSize)).toStrictEqual(['width', 'height'])
+    })
+
+    it('Should generate config', () => {
+      // const wrapper = mountDefaultYooCamera(mount)
+      const localThis = {
+        canvasSize: { width: 400, height: 400 },
+        roiWidth: 200,
+        roiHeight: 200,
+        roi: { areaRadius: 0, areaStroke: 5, areaColor: '#d1ff00' }
+      }
+
+      const x = (localThis.canvasSize.width / 2) - (localThis.roiWidth / 2)
+      const y = (localThis.canvasSize.height / 2) - (localThis.roiHeight / 2)
+
+      expect(YooCamera.computed.takeRoiConfig.call(localThis))
+        .toStrictEqual({
+          x,
+          y,
+          width: localThis.roiWidth,
+          height: localThis.roiHeight,
+          ...localThis.roi
+        })
+    })
+
+    it('Should emit finish when frame count is higher then total', () => {
+      const wrapper = mountDefaultYooCamera(mount)
+
+      wrapper.vm.doGetFrame({ total: 3, count: 3 })
+
+      expect(wrapper.emitted('finish')).toBeTruthy()
+    })
+
+    it('Should return that input is valid', () => {
+      const wrapper = mountDefaultYooCamera(mount)
+      const base64Image = new String('image')
+
+      console.log(wrapper.vm.doVerifyOutputFrame(base64Image))
+    })
+
+    it('Should change value from props', () => {
+      const wrapper = mountDefaultYooCamera(mount)
+
+      wrapper.setProps({
+        showCamera: false,
+        debug: false,
+        capture: false,
+        roi: {
+          areaRadius: 0,
+          areaStroke: 5,
+          areaColor: '#d1ff00'
+        },
+        captureAmount: 3,
+        mode: PropsConfig.mode.options[1],
+        base64: false,
+        blob: true,
+        facingMode: PropsConfig.facingMode.options[1]
+      })
+
+      expect(wrapper).toBeTruthy()
+    })
+
+    it('Should return empty object if canvasSize is empty', () => {
+      const localThis = {
+        canvasSize: { width: 0, height: 0 },
+        roiWidth: 200,
+        roiHeight: 200,
+        roi: { areaRadius: 0, areaStroke: 5, areaColor: '#d1ff00' }
+      }
+
+      expect(YooCamera.computed.takeRoiConfig.call(localThis)).toStrictEqual({})
+    })
   })
 })
