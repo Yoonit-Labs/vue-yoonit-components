591dd9eebae8981b1db50370ab5fdafb26ad87a4
Gabriel Santiago Cardoso Rizzo <gabriel96.gsr@gmail.com>
2022-02-23 17:37:51 -0300

    Reorganizing tests

--- src/components/molecules/Camera/Camera.test.js
+++ src/components/molecules/Camera/Camera.test.js
@@ -18,6 +18,7 @@ import { mount, shallowMount } from '@vue/test-utils'
 import YooCamera from '@/components/molecules/Camera/Camera.vue'
 import PropsConfig from './Camera.config'
 import { dimensionRulesEnum } from '@/enums/CanvasSize'
+import YooGetUserMedia from '@/components/quarks/GetUserMedia/GetUserMedia.vue'
 
 beforeAll(() => {
   window.MediaStream = jest.fn().mockImplementation(() => ({
@@ -58,73 +59,10 @@ describe('Testing YooCamera', () => {
   })
 
   describe('Component Logic', () => {
-    it('Should emit capture event', async () => {
-      const wrapper = mountDefaultYooCamera(mount)
-
-      wrapper.vm.$emit('frame')
-
-      await wrapper.vm.$nextTick()
-      expect(wrapper.emitted().frame).toBeTruthy()
-    })
-
-    it('Should emit finish event', async () => {
-      const wrapper = mountDefaultYooCamera(mount)
-
-      wrapper.vm.$emit('finish')
-
-      await wrapper.vm.$nextTick()
-      expect(wrapper.emitted().finish).toBeTruthy()
-    })
-
-    it('Should emit permissionDenied event', async () => {
-      const wrapper = mountDefaultYooCamera(mount)
-
-      wrapper.vm.$emit('permissionDenied')
-
-      await wrapper.vm.$nextTick()
-      expect(wrapper.emitted().permissionDenied).toBeTruthy()
-    })
-
-    it('Should return correct facing mode', () => {
-      const localThis = { facingMode: PropsConfig.facingMode.default }
-
-      expect(YooCamera.computed.takeStream.call(localThis).video.facingMode).toBe(PropsConfig.facingMode.default)
-    })
-
     it('Should set canvas size correctly', () => {
       const wrapper = mountDefaultYooCamera(mount)
 
       expect(Object.keys(wrapper.vm.canvasSize)).toStrictEqual(['width', 'height'])
-    })
-
-    it('Should generate config', () => {
-      // const wrapper = mountDefaultYooCamera(mount)
-      const localThis = {
-        canvasSize: { width: 400, height: 400 },
-        roiWidth: 200,
-        roiHeight: 200,
-        roi: { areaRadius: 0, areaStroke: 5, areaColor: '#d1ff00' }
-      }
-
-      const x = (localThis.canvasSize.width / 2) - (localThis.roiWidth / 2)
-      const y = (localThis.canvasSize.height / 2) - (localThis.roiHeight / 2)
-
-      expect(YooCamera.computed.takeRoiConfig.call(localThis))
-        .toStrictEqual({
-          x,
-          y,
-          width: localThis.roiWidth,
-          height: localThis.roiHeight,
-          ...localThis.roi
-        })
-    })
-
-    it('Should emit finish when frame count is higher then total', () => {
-      const wrapper = mountDefaultYooCamera(mount)
-
-      wrapper.vm.doGetFrame({ total: 3, count: 3 })
-
-      expect(wrapper.emitted('finish')).toBeTruthy()
     })
 
     it('Should return that input is valid', () => {
@@ -154,17 +92,6 @@ describe('Testing YooCamera', () => {
       })
 
       expect(wrapper).toBeTruthy()
-    })
-
-    it('Should return empty object if canvasSize is empty', () => {
-      const localThis = {
-        canvasSize: { width: 0, height: 0 },
-        roiWidth: 200,
-        roiHeight: 200,
-        roi: { areaRadius: 0, areaStroke: 5, areaColor: '#d1ff00' }
-      }
-
-      expect(YooCamera.computed.takeRoiConfig.call(localThis)).toStrictEqual({})
     })
 
     it('doGetVideoElm should set input element to data', () => {
@@ -230,6 +157,94 @@ describe('Testing YooCamera', () => {
           expect(wrapper.vm.$data.roiHeight).toBeTruthy()
         })
       })
+    })
+  })
+
+  describe('Component computed properties', () => {
+    it('Should return correct facing mode', () => {
+      const localThis = { facingMode: PropsConfig.facingMode.default }
+
+      expect(YooCamera.computed.takeStream.call(localThis).video.facingMode).toBe(PropsConfig.facingMode.default)
+    })
+
+    it('Should generate config', () => {
+      // const wrapper = mountDefaultYooCamera(mount)
+      const localThis = {
+        canvasSize: { width: 400, height: 400 },
+        roiWidth: 200,
+        roiHeight: 200,
+        roi: { areaRadius: 0, areaStroke: 5, areaColor: '#d1ff00' }
+      }
+
+      const x = (localThis.canvasSize.width / 2) - (localThis.roiWidth / 2)
+      const y = (localThis.canvasSize.height / 2) - (localThis.roiHeight / 2)
+
+      expect(YooCamera.computed.takeRoiConfig.call(localThis))
+        .toStrictEqual({
+          x,
+          y,
+          width: localThis.roiWidth,
+          height: localThis.roiHeight,
+          ...localThis.roi
+        })
+    })
+
+    it('Should return empty object if canvasSize is empty', () => {
+      const localThis = {
+        canvasSize: { width: 0, height: 0 },
+        roiWidth: 200,
+        roiHeight: 200,
+        roi: { areaRadius: 0, areaStroke: 5, areaColor: '#d1ff00' }
+      }
+
+      expect(YooCamera.computed.takeRoiConfig.call(localThis)).toStrictEqual({})
+    })
+  })
+
+  describe('Component Event Emition', () => {
+    it('Should receive permission denied event and pass it through', () => {
+      const wrapper = mountDefaultYooCamera(mount)
+
+      const getUserMedia = wrapper.findComponent(YooGetUserMedia)
+
+      getUserMedia.vm.$emit('permissionDenied')
+
+      expect(wrapper.emitted().permissionDenied).toBeTruthy()
+    })
+
+    it('Should emit capture event', async () => {
+      const wrapper = mountDefaultYooCamera(mount)
+
+      wrapper.vm.$emit('frame')
+
+      await wrapper.vm.$nextTick()
+      expect(wrapper.emitted().frame).toBeTruthy()
+    })
+
+    it('Should emit finish event', async () => {
+      const wrapper = mountDefaultYooCamera(mount)
+
+      wrapper.vm.$emit('finish')
+
+      await wrapper.vm.$nextTick()
+      expect(wrapper.emitted().finish).toBeTruthy()
+    })
+
+    it('Should emit permissionDenied event', async () => {
+      const wrapper = mountDefaultYooCamera(mount)
+
+      wrapper.vm.$emit('permissionDenied')
+
+      await wrapper.vm.$nextTick()
+      expect(wrapper.emitted().permissionDenied).toBeTruthy()
+    })
+
+    it('Should emit finish when frame count is higher then total', () => {
+      const wrapper = mountDefaultYooCamera(mount)
+
+      wrapper.vm.doGetFrame({ total: 3, count: 3 })
+
+      expect(wrapper.emitted('finish')).toBeTruthy()
     })
   })
 })
